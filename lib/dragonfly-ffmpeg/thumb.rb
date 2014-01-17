4bc7cc4ac614063205adff56d1f9b8a6cb5c168e
Luca Zago <luca.zago@gmail.com>
2014-01-17 18:14:55 +0000

    video thumb

--- lib/dragonfly-ffmpeg/thumb.rb
+++ lib/dragonfly-ffmpeg/thumb.rb
@@ -6,7 +6,7 @@ module EnMasse
         def call(content, opts={})
           ext = "png"
           tempfile = Utils.new_tempfile(ext)
-          ::FFMPEG::Movie.new(content.path).screenshot(tempfile.path, seek_time: 5, resolution: '320x240')
+          FFMPEG::Movie.new(content.path).screenshot(tempfile.path, seek_time: 5, resolution: '320x240')
           update(tempfile)
           content.meta['format'] = ext
           content.ext = ext
