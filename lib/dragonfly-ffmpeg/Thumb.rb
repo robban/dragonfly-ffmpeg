6263419d9d430c58a1da670607a0bd77fa80e402
Luca Zago <luca.zago@gmail.com>
2014-01-17 17:58:53 +0000

    video thumb

---
+++ lib/dragonfly-ffmpeg/Thumb.rb
@@ -0,0 +1,18 @@
+module EnMasse
+  module Dragonfly
+    module FFMPEG
+      class Thumb
+
+        def call(content, opts={})
+          ext = "png"
+          tempfile = Utils.new_tempfile(ext)
+          ::FFMPEG::Movie.new(content.path).screenshot(tempfile.path, seek_time: 5, resolution: '320x240')
+          update(tempfile)
+          content.meta['format'] = ext
+          content.ext = ext
+        end
+
+      end
+    end
+  end
+end
