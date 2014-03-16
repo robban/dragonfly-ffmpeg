cae93e8ef2edd172ee2ae3b07b5e001a8958cf5d
TradeMobile Ltd <bertie@trade-mobile.com>
2014-03-16 18:17:39 +0000

    Update extract.rb

--- lib/dragonfly-ffmpeg/extract.rb
+++ lib/dragonfly-ffmpeg/extract.rb
@@ -4,9 +4,6 @@ module EnMasse
       class Extract
 
         def call(content, args='', opts={})
-          Rails.logger.debug("£££££ FFMPEG thumb")
-          Rails.logger.debug(args['seconds'].to_s)
-          Rails.logger.debug(args['geometry'] .to_s)
           ext = "png"
           tempfile = ::Dragonfly::Utils.new_tempfile(ext)
           ::FFMPEG::Movie.new(content.path).screenshot(tempfile.path, seek_time: args['seconds'], resolution: args['geometry'])
